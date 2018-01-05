668189d228c0995101498e75fa0a0dedccc1e4cb
jQrgen <jorgen.notland@protonmail.com>
2018-01-05 17:50:28 +0200

    requestNotificationAuthorization

--- src/ios/MyCordovaPlugin.m
+++ src/ios/MyCordovaPlugin.m
@@ -1,10 +1,22 @@
 #import "notificationAuthorization.h"
 
 #import <Cordova/CDVAvailability.h>
+#import <PushKit/PushKit.h>
+#import <UserNotifications/UserNotifications.h>
 
 @implementation notificationAuthorization
 
 - (void)pluginInitialize {
+}
+
+-(void)requestNotificationAuthorization:(CDVInvokedUrlCommand *)command {
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate = self;
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (!error) {
+            NSLog(@"request authorization succeeded!");
+        }
+    }];
 }
 
 - (void)echo:(CDVInvokedUrlCommand *)command {
